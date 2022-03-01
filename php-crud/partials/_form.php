eab58e6e03e30010201ed739b04dac885119355c
Takayuki Miura <6015abc@gmail.com>
2022-03-02 02:15:19 +0900

    見た目の整形

--- php-crud/partials/_form.php
+++ php-crud/partials/_form.php
@@ -9,6 +9,7 @@
         <?php endif ?>
       </h3>
     </div>
+
     <div class="card-body">
       <form action="" method="POST" enctype="multipart/form-data">
 
@@ -16,21 +17,26 @@
           <label class="form-label">ID</label>
           <input name="id" value="<?php echo $web_page_data['id']; ?>" class="form-control">
         </div>
+
         <div class="form-group mb-3">
           <label class="form-label">CompanyName</label>
           <input name="companyName" value="<?php echo $web_page_data['companyName']; ?>" class="form-control">
         </div>
+
         <div class="form-group mb-3">
           <label class="form-label">URL</label>
           <input name="url" value="<?php echo $web_page_data['url']; ?>" class="form-control">
         </div>
+
         <div class="form-group mb-3">
           <label class="form-label">ScrollFlag：true or false</label>
           <input name="scrollFlag" value="<?php echo $web_page_data['scrollFlag'] ? "true" : "false"; ?>" class="form-control">
         </div>
 
         <button class="btn btn-success">Submit</button>
+
       </form>
     </div>
+
   </div>
 </div>
